4ce69265d685b5b8c1cf952023d97f103781e00d
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-09-01 01:53:22 -0400

    02.py- print numbers from 10 to 1 using while loop

--- loop/beginner/01.py
+++ loop/beginner/01.py
@@ -1,1 +1,2 @@
+#Print numbers from 1 to 10 using a for loop.
 for i in range(1, 11): print(i)
---
+++ loop/beginner/02.py
@@ -0,0 +1,5 @@
+#Print numbers from 10 to 1 using a while loop.
+num=10
+while(num>=1):
+    print(num)
+    num=num-1
