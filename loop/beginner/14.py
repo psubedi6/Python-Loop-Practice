fc6306de9c28f68f1b8ca5e108f8f4f4e0ebd86c
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-09-01 17:30:59 -0400

    Update beginner/14.py

--- loop/beginner/14.py
+++ loop/beginner/14.py
@@ -1,8 +1,7 @@
 #Count the digits in a number using a loop.
 num=int(input("Enter the number: "))
-rev=0
+count=0
 while num>0:
-    digit = num%10
-    rev=rev+1
+    count=count+1
     num=num//10
-print (rev)
+print (count)
