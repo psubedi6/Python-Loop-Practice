e048e968e27076cc20dab755e833df684fcdb858
Prakash Subedi <prakashsubedi@Prakashs-MacBook-Air.local>
2025-09-03 21:56:39 -0400

    Add 10.py-take inputs until the user enters stop

---
+++ loop/intermediate/10.py
@@ -0,0 +1,8 @@
+#Take inputs until the user enters "stop".
+i=0
+while i==0:
+    word= input("Enter a input: ")
+    print("Enter \"stop\" if you wanna stop")
+    if(word=="stop"):
+        break
+    
